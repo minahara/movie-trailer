0bceae1a667e38cc97db9bd4f4ee07320147c433
ewuweblab <pmanikoth@ewu.edu>
2017-02-15 12:23:17 -0800

    Clean file & Syntax errors
    * remember to close media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // Scripts
-$('h1').lettering();
+//$('h1').lettering();
 
 
-$('video').mediaElement();
+//$('video').mediaElement();
 
 
-$('h1').fitText();
+//$('h1').fitText();
 
+/* 
 $('h1').arctext({
   dir : 1,
   rotate: true,
   radius: 500
 });
+*/
